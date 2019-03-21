376a502f3e69d09d79ee570bbdbd195f1fa0b04f
Riyandi syahputra <riyandisyahputra97@gmail.com>
2019-03-21 13:11:20 +0700

    Add files via upload

---
+++ exercise3/script.js
@@ -0,0 +1,415 @@
+var obj= 
+[ {
+    "name": "Alderaan", 
+    "rotation_period": "24", 
+    "orbital_period": "364", 
+    "diameter": "12500", 
+    "climate": "temperate", 
+    "gravity": "1 standard", 
+    "terrain": "grasslands, mountains", 
+    "surface_water": "40", 
+    "population": "2000000000", 
+    "residents": [
+        "https://swapi.co/api/people/5/", 
+        "https://swapi.co/api/people/68/", 
+        "https://swapi.co/api/people/81/"
+    ], 
+    "films": [
+        "https://swapi.co/api/films/6/", 
+        "https://swapi.co/api/films/1/"
+    ], 
+    "created": "2014-12-10T11:35:48.479000Z", 
+    "edited": "2014-12-20T20:58:18.420000Z", 
+    "url": "https://swapi.co/api/planets/2/"
+}, 
+{
+    "name": "Yavin IV", 
+    "rotation_period": "24", 
+    "orbital_period": "4818", 
+    "diameter": "10200", 
+    "climate": "temperate, tropical", 
+    "gravity": "1 standard", 
+    "terrain": "jungle, rainforests", 
+    "surface_water": "8", 
+    "population": "1000", 
+    "residents": [], 
+    "films": [
+        "https://swapi.co/api/films/1/"
+    ], 
+    "created": "2014-12-10T11:37:19.144000Z", 
+    "edited": "2014-12-20T20:58:18.421000Z", 
+    "url": "https://swapi.co/api/planets/3/"
+}, 
+{
+    "name": "Hoth", 
+    "rotation_period": "23", 
+    "orbital_period": "549", 
+    "diameter": "7200", 
+    "climate": "frozen", 
+    "gravity": "1.1 standard", 
+    "terrain": "tundra, ice caves, mountain ranges", 
+    "surface_water": "100", 
+    "population": "unknown", 
+    "residents": [], 
+    "films": [
+        "https://swapi.co/api/films/2/"
+    ], 
+    "created": "2014-12-10T11:39:13.934000Z", 
+    "edited": "2014-12-20T20:58:18.423000Z", 
+    "url": "https://swapi.co/api/planets/4/"
+}, 
+{
+    "name": "Dagobah", 
+    "rotation_period": "23", 
+    "orbital_period": "341", 
+    "diameter": "8900", 
+    "climate": "murky", 
+    "gravity": "N/A", 
+    "terrain": "swamp, jungles", 
+    "surface_water": "8", 
+    "population": "unknown", 
+    "residents": [], 
+    "films": [
+        "https://swapi.co/api/films/2/", 
+        "https://swapi.co/api/films/6/", 
+        "https://swapi.co/api/films/3/"
+    ], 
+    "created": "2014-12-10T11:42:22.590000Z", 
+    "edited": "2014-12-20T20:58:18.425000Z", 
+    "url": "https://swapi.co/api/planets/5/"
+}, 
+{
+    "name": "Bespin", 
+    "rotation_period": "12", 
+    "orbital_period": "5110", 
+    "diameter": "118000", 
+    "climate": "temperate", 
+    "gravity": "1.5 (surface), 1 standard (Cloud City)", 
+    "terrain": "gas giant", 
+    "surface_water": "0", 
+    "population": "6000000", 
+    "residents": [
+        "https://swapi.co/api/people/26/"
+    ], 
+    "films": [
+        "https://swapi.co/api/films/2/"
+    ], 
+    "created": "2014-12-10T11:43:55.240000Z", 
+    "edited": "2014-12-20T20:58:18.427000Z", 
+    "url": "https://swapi.co/api/planets/6/"
+}, 
+{
+    "name": "Endor", 
+    "rotation_period": "18", 
+    "orbital_period": "402", 
+    "diameter": "4900", 
+    "climate": "temperate", 
+    "gravity": "0.85 standard", 
+    "terrain": "forests, mountains, lakes", 
+    "surface_water": "8", 
+    "population": "30000000", 
+    "residents": [
+        "https://swapi.co/api/people/30/"
+    ], 
+    "films": [
+        "https://swapi.co/api/films/3/"
+    ], 
+    "created": "2014-12-10T11:50:29.349000Z", 
+    "edited": "2014-12-20T20:58:18.429000Z", 
+    "url": "https://swapi.co/api/planets/7/"
+}, 
+{
+    "name": "Naboo", 
+    "rotation_period": "26", 
+    "orbital_period": "312", 
+    "diameter": "12120", 
+    "climate": "temperate", 
+    "gravity": "1 standard", 
+    "terrain": "grassy hills, swamps, forests, mountains", 
+    "surface_water": "12", 
+    "population": "4500000000", 
+    "residents": [
+        "https://swapi.co/api/people/3/", 
+        "https://swapi.co/api/people/21/", 
+        "https://swapi.co/api/people/36/", 
+        "https://swapi.co/api/people/37/", 
+        "https://swapi.co/api/people/38/", 
+        "https://swapi.co/api/people/39/", 
+        "https://swapi.co/api/people/42/", 
+        "https://swapi.co/api/people/60/", 
+        "https://swapi.co/api/people/61/", 
+        "https://swapi.co/api/people/66/", 
+        "https://swapi.co/api/people/35/"
+    ], 
+    "films": [
+        "https://swapi.co/api/films/5/", 
+        "https://swapi.co/api/films/4/", 
+        "https://swapi.co/api/films/6/", 
+        "https://swapi.co/api/films/3/"
+    ], 
+    "created": "2014-12-10T11:52:31.066000Z", 
+    "edited": "2014-12-20T20:58:18.430000Z", 
+    "url": "https://swapi.co/api/planets/8/"
+}, 
+{
+    "name": "Coruscant", 
+    "rotation_period": "24", 
+    "orbital_period": "368", 
+    "diameter": "12240", 
+    "climate": "temperate", 
+    "gravity": "1 standard", 
+    "terrain": "cityscape, mountains", 
+    "surface_water": "unknown", 
+    "population": "1000000000000", 
+    "residents": [
+        "https://swapi.co/api/people/34/", 
+        "https://swapi.co/api/people/55/", 
+        "https://swapi.co/api/people/74/"
+    ], 
+    "films": [
+        "https://swapi.co/api/films/5/", 
+        "https://swapi.co/api/films/4/", 
+        "https://swapi.co/api/films/6/", 
+        "https://swapi.co/api/films/3/"
+    ], 
+    "created": "2014-12-10T11:54:13.921000Z", 
+    "edited": "2014-12-20T20:58:18.432000Z", 
+    "url": "https://swapi.co/api/planets/9/"
+}, 
+{
+    "name": "Kamino", 
+    "rotation_period": "27", 
+    "orbital_period": "463", 
+    "diameter": "19720", 
+    "climate": "temperate", 
+    "gravity": "1 standard", 
+    "terrain": "ocean", 
+    "surface_water": "100", 
+    "population": "1000000000", 
+    "residents": [
+        "https://swapi.co/api/people/22/", 
+        "https://swapi.co/api/people/72/", 
+        "https://swapi.co/api/people/73/"
+    ], 
+    "films": [
+        "https://swapi.co/api/films/5/"
+    ], 
+    "created": "2014-12-10T12:45:06.577000Z", 
+    "edited": "2014-12-20T20:58:18.434000Z", 
+    "url": "https://swapi.co/api/planets/10/"
+}, 
+{
+    "name": "Geonosis", 
+    "rotation_period": "30", 
+    "orbital_period": "256", 
+    "diameter": "11370", 
+    "climate": "temperate, arid", 
+    "gravity": "0.9 standard", 
+    "terrain": "rock, desert, mountain, barren", 
+    "surface_water": "5", 
+    "population": "100000000000", 
+    "residents": [
+        "https://swapi.co/api/people/63/"
+    ], 
+    "films": [
+        "https://swapi.co/api/films/5/"
+    ], 
+    "created": "2014-12-10T12:47:22.350000Z", 
+    "edited": "2014-12-20T20:58:18.437000Z", 
+    "url": "https://swapi.co/api/planets/11/"
+
+}];
+
+var head = "<table class='table table-dark' style='background-color:#0288d1;' id='tbl-id'><thead ><tr class='tr-edit'><td><input type='text' id='name-id' class='btn btn-light'></td><td><input type='number' id='rot-id' class='btn btn-light'></td><td><input type='number' id='orb-id' class='btn btn-light'></td><td><input type='number' id='diam-id' class='btn btn-light'></td><td><input type='text' id='surf-id' class='btn btn-light'></td></tr><tr class='table table-light'><th>Name</th><th>Rotation Period</th><th>Orbit Period</th><th>Diameter</th><th>Surface Water</th></tr></thead><tbody id='tbody-id'>"
+var str = "";
+for (var i = 0; i < obj.length; i++) {
+    str = str + "<tr class='tr-output'><td class='value-name'><p>" + obj[i].name + "</p></td><td class='value-rot'><p>" + obj[i].rotation_period + "</p></td><td class='value-orb'><p>" + obj[i].orbital_period + "</p></td><td class='value-diam'><p>" + obj[i].diameter + "</p></td><td class='value-surf'><p>" + obj[i].surface_water + "</td></tr>";
+}
+head = head + str + "<t/body></table>";
+document.getElementById("tampil").innerHTML = head;
+
+var filterInput = document.getElementById("name-id");
+if (filterInput) {
+    filterInput.addEventListener('keyup', filterName);
+
+    function filterName() {
+        var filterValue = document.getElementById('name-id').value.toUpperCase();
+        var table = document.getElementById('tbl-id');
+        var td = table.querySelectorAll('td.value-name');
+        var tr = table.querySelectorAll('tr.tr-output');
+        var i;
+
+        for (i = 0; i < td.length; i++) {
+            var a = td[i].getElementsByTagName('p')[0];
+            if (a.innerHTML.toUpperCase().indexOf(filterValue) > -1) {
+                tr[i].style.display = '';
+                document.getElementById("rot-id").disabled = false;
+                document.getElementById("orb-id").disabled = false;
+                document.getElementById("diam-id").disabled = false;
+                document.getElementById("surf-id").disabled = false;
+            }
+            else {
+                tr[i].style.display = 'none';
+                document.getElementById("rot-id").disabled = true;
+                document.getElementById("orb-id").disabled = true;
+                document.getElementById("diam-id").disabled = true;
+                document.getElementById("surf-id").disabled = true;
+            }
+            if (a.innerHTML.toUpperCase().indexOf(filterValue) > -1) {
+                tr[i].style.display = '';
+            }
+            else {
+                tr[i].style.display = 'none';
+            }
+        }
+    }
+}
+var filterInput2 = document.getElementById("rot-id");
+if (filterInput2) {
+    filterInput2.addEventListener('keyup', filterRot);
+
+    function filterRot() {
+        var filterValue = document.getElementById('rot-id').value.toUpperCase();
+        var table = document.getElementById('tbl-id');
+        var td = table.querySelectorAll('td.value-rot');
+        var tr = table.querySelectorAll('tr.tr-output');
+        var i;
+
+        for (i = 0; i < td.length; i++) {
+            var a = td[i].getElementsByTagName('p')[0];
+            if (a.innerHTML.toUpperCase().indexOf(filterValue) > -1) {
+                tr[i].style.display = '';
+                document.getElementById("name-id").disabled = false;
+                document.getElementById("orb-id").disabled = false;
+                document.getElementById("diam-id").disabled = false;
+                document.getElementById("surf-id").disabled = false;
+            }
+            else {
+                tr[i].style.display = 'none';
+                document.getElementById("name-id").disabled = true;
+                document.getElementById("orb-id").disabled = true;
+                document.getElementById("diam-id").disabled = true;
+                document.getElementById("surf-id").disabled = true;
+            }
+            if (a.innerHTML.toUpperCase().indexOf(filterValue) > -1) {
+                tr[i].style.display = '';
+            }
+            else {
+                tr[i].style.display = 'none';
+            }
+        }
+    }
+}
+
+var filterInput3 = document.getElementById("orb-id");
+if (filterInput3) {
+    filterInput3.addEventListener('keyup', filterOrb);
+
+    function filterOrb() {
+        var filterValue = document.getElementById('orb-id').value.toUpperCase();
+        var table = document.getElementById('tbl-id');
+        var td = table.querySelectorAll('td.value-orb');
+        var tr = table.querySelectorAll('tr.tr-output');
+        var i;
+
+        for (i = 0; i < td.length; i++) {
+            var a = td[i].getElementsByTagName('p')[0];
+            if (a.innerHTML.toUpperCase().indexOf(filterValue) > -1) {
+                tr[i].style.display = '';
+                document.getElementById("name-id").disabled = false;
+                document.getElementById("rot-id").disabled = false;
+                document.getElementById("diam-id").disabled = false;
+                document.getElementById("surf-id").disabled = false;
+            }
+            else {
+                tr[i].style.display = 'none';
+                document.getElementById("name-id").disabled = true;
+                document.getElementById("rot-id").disabled = true;
+                document.getElementById("diam-id").disabled = true;
+                document.getElementById("surf-id").disabled = true;
+            }
+            var a = td[i].getElementsByTagName('p')[0];
+
+            if (a.innerHTML.toUpperCase().indexOf(filterValue) > -1) {
+                tr[i].style.display = '';
+            }
+            else {
+                tr[i].style.display = 'none';
+            }
+        }
+    }
+}
+
+var filterInput4 = document.getElementById("diam-id");
+if (filterInput4) {
+    filterInput4.addEventListener('keyup', filterDiam);
+
+    function filterDiam() {
+        var filterValue = document.getElementById('diam-id').value.toUpperCase();
+        var table = document.getElementById('tbl-id');
+        var td = table.querySelectorAll('td.value-diam');
+        var tr = table.querySelectorAll('tr.tr-output');
+        var i;
+
+        for (i = 0; i < td.length; i++) {
+            var a = td[i].getElementsByTagName('p')[0];
+            if (a.innerHTML.toUpperCase().indexOf(filterValue) > -1) {
+                tr[i].style.display = '';
+                document.getElementById("rot-id").disabled = false;
+                document.getElementById("orb-id").disabled = false;
+                document.getElementById("name-id").disabled = false;
+                document.getElementById("surf-id").disabled = false;
+            }
+            else {
+                tr[i].style.display = 'none';
+                document.getElementById("rot-id").disabled = true;
+                document.getElementById("orb-id").disabled = true;
+                document.getElementById("name-id").disabled = true;
+                document.getElementById("surf-id").disabled = true;
+            }
+            if (a.innerHTML.toUpperCase().indexOf(filterValue) > -1) {
+                tr[i].style.display = '';
+            }
+            else {
+                tr[i].style.display = 'none';
+            }
+        }
+    }
+}
+
+var filterInput5 = document.getElementById("surf-id");
+if (filterInput5) {
+    filterInput5.addEventListener('keyup', filterSurf);
+
+    function filterSurf() {
+        var filterValue = document.getElementById('surf-id').value.toUpperCase();
+        var table = document.getElementById('tbl-id');
+        var td = table.querySelectorAll('td.value-surf');
+        var tr = table.querySelectorAll('tr.tr-output');
+        var i;
+
+        for (i = 0; i < td.length; i++) {
+            var a = td[i].getElementsByTagName('p')[0];
+            if (a.innerHTML.toUpperCase().indexOf(filterValue) > -1) {
+                tr[i].style.display = '';
+                document.getElementById("rot-id").disabled = false;
+                document.getElementById("orb-id").disabled = false;
+                document.getElementById("diam-id").disabled = false;
+                document.getElementById("name-id").disabled = false;
+            }
+            else {
+                tr[i].style.display = 'none';
+                document.getElementById("rot-id").disabled = true;
+                document.getElementById("orb-id").disabled = true;
+                document.getElementById("diam-id").disabled = true;
+                document.getElementById("name-id").disabled = true;
+            }
+            if (a.innerHTML.toUpperCase().indexOf(filterValue) > -1) {
+                tr[i].style.display = '';
+            }
+            else {
+                tr[i].style.display = 'none';
+            }
+        }
+    }
+}
